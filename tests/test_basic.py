99412fa4f33000abb4abfcf905e3fccec0028f88
praneshp <praneshpg@gmail.com>
2016-03-21 23:13:23 -0700

    Create struture

---
+++ tests/test_basic.py
@@ -0,0 +1,9 @@
+from .context import sample
+import unittest
+
+class BasicTest(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def test_something(self):
+        self.assertEquals(2, 2)
